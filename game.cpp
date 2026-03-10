53fa9c9206f9d4665d0b8b10c3dad4e253a92b0f
Sroemkhu <Sroemkhunnn@gmail.com>
2026-03-10 15:47:53 +0700

    completed

--- game.cpp
+++ game.cpp
@@ -1,50 +1,125 @@
-#include <iostream>
 #include "game.h"
 #include "slot.h"
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
-void playGame(Player &player)
-{
-    while(player.token > 0)
-    {
+void saveHistory(string r[3]){
+    ofstream file("history.txt", ios::app);
+    file << r[0] << " " << r[1] << " " << r[2] << endl;
+}
+
+void showSlot(string r[3]){
+    cout << "-----------------\n";
+    cout << r[0] << " | " << r[1] << " | " << r[2] << endl;
+    cout << "-----------------\n";
+}
+
+int checkReward(string r[3], int bet){
+
+    if(r[0] == r[1] && r[1] == r[2]){
+
+        if(r[0] == "7"){
+            cout << "JACKPOT 777 !!!\n";
+            return bet * 10;
+        }
+
+        cout << "Triple Match!\n";
+        return bet * 5;
+    }
+
+    else if(r[0] == r[1] || r[1] == r[2] || r[0] == r[2]){
+        cout << "Double match!\n";
+        return bet * 2;
+    }
+
+    cout << "Lose\n";
+    return -bet;
+}
+
+int doubleOrNothing(int reward, int bet){
+
+    char choice;
+    cout << "Double or Nothing? (y/n): ";
+    cin >> choice;
+
+    if(choice != 'y'){
+        return reward;
+    }
+
+    int n = rand() % 10;
+
+    if(n % 2 == 0){
+        cout << "Lucky! Reward x2\n";
+        return reward * 2;
+    }
+    else{
+        cout << "Lose everything!\n";
+        return -(bet * 2);
+    }
+}
+
+void playGame(){
+
+    srand(time(0));
+
+    int token = 100;
+    int round = 0;
+
+    while(token > 0){
+
+        cout << "\nToken: " << token << endl;
+
+        char play;
+        cout << "Play? (y/n): ";
+        cin >> play;
+
+        if(play != 'y'){
+            break;
+        }
+
         int bet;
+        cout << "Bet token: ";
+        cin >> bet;
 
-        cout<<"\nToken : "<<player.token<<endl;
-        cout<<"Bet : ";
-        cin>>bet;
-
-        if(bet > player.token)
-        {
-            cout<<"Not enough token\n";
+        if(bet > token){
+            cout << "Not enough token\n";
             continue;
         }
 
-        player.token -= bet;
+        cout << "\nSpinning...\n";
+        this_thread::sleep_for(chrono::milliseconds(500));
 
-        string s1 = spinSlot();
-        string s2 = spinSlot();
-        string s3 = spinSlot();
+        string result[3];
 
-        cout<<"["<<s1<<"] ["<<s2<<"] ["<<s3<<"]\n";
+        spinSlot(result);
 
-        if(s1==s2 && s2==s3)
-        {
-            cout<<"JACKPOT!\n";
-            player.token += bet*5;
-        }
-        else if(s1==s2 || s2==s3 || s1==s3)
-        {
-            cout<<"WIN!\n";
-            player.token += bet*2;
-        }
-        else
-        {
-            cout<<"LOSE\n";
+        cout << "Result: ["
+            << result[0] << " | "
+            << result[1] << " | "
+            << result[2] << "]\n";
+
+        int reward = checkReward(result, bet);
+
+        if(reward > 0){
+            reward = doubleOrNothing(reward, bet);
         }
 
-        player.round++;
+        token += reward;
+
+        saveHistory(result);
+
+        round++;
+
+        if(round % 5 == 0){
+            cout << "Lucky Level Bonus +10 token\n";
+            token += 10;
+        }
     }
 
-    cout<<"\nGame Over\n";
+    cout << "\nGame Over\n";
 }
