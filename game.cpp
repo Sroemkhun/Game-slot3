8b194ac647d9c694be7b5aac49df87b8b973bdf6
Sroemkhu <Sroemkhunnn@gmail.com>
2026-03-10 14:10:54 +0700

    completed

--- game.cpp
+++ game.cpp
@@ -46,31 +46,23 @@ void playGame(Player &p){
         cout << "\n========================\n";
         cout << "Token: " << p.token << endl;
         cout << "Boss money: " << bossBank << endl;
-
         cout << "Play? (y/n): ";
         cin >> play;
-
         if(play != 'y' && play != 'Y') break;
-
         int bet;
         cout << "Bet token: ";
         cin >> bet;
-
         if(bet > p.token){
             cout << "Not enough token\n";
             continue;
         }
         p.token -= bet;
-
         cout << "\nSpinning...\n";
         this_thread::sleep_for(chrono::milliseconds(700));
-
         spinSlot(slot);
         showSlot(slot);
         saveHistory(slot);
-
         int reward = 0;
-
         if(slot[0] == slot[1] && slot[1] == slot[2]){
             if(slot[0] == "7") reward = bet * 10;
             else reward = bet * 5;
@@ -84,25 +76,19 @@ void playGame(Player &p){
             reward = -bet;
             cout << "Lose!\n";
         }
-
         if(reward > 0) {
             doubleOrNothing(reward);
         }
-
         p.token += reward;
         bossBank -= reward;
-
         cout << "Result Score: " << reward << endl;
-
         if(p.token > p.maxToken) p.maxToken = p.token;
         p.round++;
-
         if(p.round % 5 == 0){
             cout << "Lucky Level! +20 token\n";
             p.token += 20;
         }
     }
-
     cout << "\nGame Over\n";
     cout << "Max token: " << p.maxToken << endl;
 }
